27f3bdea396139e3abccb45ec1df4d9231bd0bc7
JiazeWang <jiazewang@csu.edu.cn>
2019-03-19 01:52:46 +0800

    1

--- wider_extract.py
+++ wider_extract.py
@@ -128,9 +128,9 @@ if __name__ == '__main__':
                         help="height of an image (default: 256)")
     parser.add_argument('--width', type=int, default=128,
                         help="width of an image (default: 128)")
-    parser.add_argument('--test-batch', default=1, type=int, help="test batch size")
+    parser.add_argument('--test-batch', default=64, type=int, help="test batch size")
     parser.add_argument('-a', '--arch', type=str, default='densenet121', choices=models.get_names())
-    parser.add_argument('--seed', type=int, default=64, help="manual seed")
+    parser.add_argument('--seed', type=int, default=1, help="manual seed")
     parser.add_argument('--gpu', default='0', type=str, help='gpu device ids for CUDA_VISIBLE_DEVICES')
     args = parser.parse_args()
     main(args)
